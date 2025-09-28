d2d7f9865793a4ca45bd69a28327c860c99845e3
Nukiflex <ytnecrex@gmail.com>
2025-09-28 15:53:38 +0200

    Initial commit

---
+++ src/main/java/com/nukiflex/infinitycastle/InfinityCastleClient.java
@@ -0,0 +1,10 @@
+package com.nukiflex.infinitycastle;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class InfinityCastleClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/nukiflex/infinitycastle/InfinityCastleDataGenerator.java
@@ -0,0 +1,11 @@
+package com.nukiflex.infinitycastle;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class InfinityCastleDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
